237dfe03d3d166855d1ce0e7249acc58d969d314
dailydataminer <benjaminmartin@theitguy.me>
2019-09-22 16:02:43 -0700

    I caved and removed the ;...

--- JumJumJr/jumjumjr.py
+++ JumJumJr/jumjumjr.py
@@ -1,55 +1,55 @@
-import configparser;
-import json;
-import logging;
-import os;
-import requests;
-import sys;
+import configparser 
+import json 
+import logging
+import os
+import requests 
+import sys 
 
 def get_twitch_data(session, config):
 
     ## Basic stats
-    file_name = 'jumjumjr.py';
+    file_name = 'jumjumjr.py' 
 
     ## Sets up pathing for log file
-    current_directory = os.path.dirname(__file__);
-    parent = os.path.dirname(current_directory);
-    home_dir = os.path.join(parent, 'logs');
-    log_path = os.path.join(home_dir, 'log.txt');
+    current_directory = os.path.dirname(__file__) 
+    parent = os.path.dirname(current_directory) 
+    home_dir = os.path.join(parent, 'logs') 
+    log_path = os.path.join(home_dir, 'log.txt') 
 
-    logging.info("Entering " + file_name);
+    logging.info("Entering " + file_name) 
 
     ## Base headers
-    headers = {'Client-ID': config['TwitchAuth']['ClientID']};
-    params = {'user_login': config['TwitchStream']['Name']};
-    params_host = {'login': config['TwitchStream']['Name']};
+    headers = {'Client-ID': config['TwitchAuth']['ClientID']} 
+    params = {'user_login': config['TwitchStream']['Name']} 
+    params_host = {'login': config['TwitchStream']['Name']} 
 
     ## Base URLs
-    stream_URL = 'https://api.twitch.tv/helix/streams';
-    ID_URL = 'https://api.twitch.tv/helix/users';
-    game_URL = 'https://api.twitch.tv/helix/games';
-    host_URL = 'https://tmi.twitch.tv/hosts';
-    twitch_url = 'https://twitch.tv/';
+    stream_URL = 'https://api.twitch.tv/helix/streams' 
+    ID_URL = 'https://api.twitch.tv/helix/users' 
+    game_URL = 'https://api.twitch.tv/helix/games' 
+    host_URL = 'https://tmi.twitch.tv/hosts' 
+    twitch_url = 'https://twitch.tv/' 
 
     ## Get request to Twitch. Will return null if not streaming
-    response = session.get(stream_URL, params=params, headers=headers);
+    response = session.get(stream_URL, params=params, headers=headers) 
 
     ## Who doesn't like a good logging?
     if response.status_code == 200:
-        logging.info("Request successful.");
+        logging.info("Request successful.") 
     else:
-        logging.error("Request failed. %s", response.json());
+        logging.error("Request failed. %s", response.json()) 
 
     ## First will attempt to get his live data
     try:
         ## Retrieves value from first GET that will fail if he is not streaming
-        viewer_count = response.json()['data'][0]['viewer_count'];
+        viewer_count = response.json()['data'][0]['viewer_count'] 
 
         ## Gets game ID and prepares the GET call to retrieve game information
-        game_id = response.json()['data'][0]['game_id'];
-        game_params = {'id': game_id};
-        response_game = session.get(game_URL, params=game_params, headers=headers);
+        game_id = response.json()['data'][0]['game_id'] 
+        game_params = {'id': game_id} 
+        response_game = session.get(game_URL, params=game_params, headers=headers) 
         ## Retrieves values of game
-        game_name = response_game.json()['data'][0]['name'];
+        game_name = response_game.json()['data'][0]['name'] 
         box_art_url_template = response_game.json()['data'][0]['box_art_url']
         box_art_url = box_art_url_template.format(width=100, height=100)
         # We now have stream information
@@ -57,7 +57,7 @@ def get_twitch_data(session, config):
                        'url': twitch_url + config['TwitchStream']['Name'],
                        'game_name': game_name,
                        'game_box_art': box_art_url,
-                       'viewer_count': viewer_count};
+                       'viewer_count': viewer_count} 
 
     # Exception will occur if he is not streaming
     # Will now check if he is hosting
@@ -65,51 +65,51 @@ def get_twitch_data(session, config):
         try:
 
             ## Have to now retrieve his ID
-            response_host = session.get(ID_URL, params=params_host, headers=headers);
-            streamer_id = response_host.json()['data'][0]['id'];
+            response_host = session.get(ID_URL, params=params_host, headers=headers) 
+            streamer_id = response_host.json()['data'][0]['id'] 
 
             host_params = {'include_logins': '1',
-                       'host': streamer_id};
+                       'host': streamer_id} 
 
             # Foreign API used to obtain hosting information
-            response_host = session.get(host_URL, params=host_params);
+            response_host = session.get(host_URL, params=host_params) 
             
             if response_host.status_code == 200:
-                logging.info("Request successful.");
+                logging.info("Request successful.") 
             else:
-                logging.error("Request failed. %s", response_host.json());
+                logging.error("Request failed. %s", response_host.json()) 
 
-            target = response_host.json()['hosts'][0]['target_login'];
+            target = response_host.json()['hosts'][0]['target_login'] 
 
             # We now have the name of the streamer he is hosting.
             twitch_data = {'status': 'Hosting',
                            'host': target,
-                           'url': twitch_url + target};
+                           'url': twitch_url + target} 
 
         ## An error here means Projared is asleep :)
         except:
-            twitch_data = {'status': 'Offline'};
+            twitch_data = {'status': 'Offline'} 
 
-    logging.info("Exiting " + file_name);
+    logging.info("Exiting " + file_name) 
 
-    return twitch_data;
+    return twitch_data 
 
 def data():
 
-    current_directory = os.path.dirname(__file__);
+    current_directory = os.path.dirname(__file__) 
     parent_directory = os.path.split(current_directory)[0]
-    file_path = os.path.join(parent_directory, 'config.ini');
+    file_path = os.path.join(parent_directory, 'config.ini') 
 
     with open(file_path) as f:
         config = configparser.ConfigParser()
         config.read_file(f)
 
-        session = requests.Session();
+        session = requests.Session() 
 
-        twitch_data = get_twitch_data(config=config, session=session);
+        twitch_data = get_twitch_data(config=config, session=session) 
 
-        return twitch_data;
+        return twitch_data 
 
 if __name__ == '__main__':
-    data();
+    data() 
 
--- projared.py
+++ projared.py
@@ -1,72 +1,71 @@
-import os;
-import json;
-import logging;
+import os
+import json
+import logging
 from pathlib import Path
-import sys;
+import sys
 
-from JumJumJr import jumjumjr;
-
+from JumJumJr import jumjumjr
 
 def create_paths():
 
     ## Log Directory & File
-    log_dir = Path.cwd() / "logs";
-    log_path = Path.cwd() / "logs" / "log.txt";
-    os.makedirs(log_dir, exist_ok=True);
+    log_dir = Path.cwd() / "logs" 
+    log_path = Path.cwd() / "logs" / "log.txt" 
+    os.makedirs(log_dir, exist_ok=True) 
 
     ## Status Directory & File
-    status_dir = Path.cwd() / "status";
-    status_path = Path.cwd() / "status" / "status.json";
-    os.makedirs(status_dir, exist_ok=True);
+    status_dir = Path.cwd() / "status" 
+    status_path = Path.cwd() / "status" / "status.json" 
+    os.makedirs(status_dir, exist_ok=True) 
 
-    return log_path, status_path;
+    return log_path, status_path 
 
 
 def read_previous_status(status_path):
     try:
-        text_data = status_path.read_text();
-        json_data = json.loads(text_data);
+        text_data = status_path.read_text() 
+        json_data = json.loads(text_data) 
 
         return json_data
     except FileNotFoundError:
         ## If no file present, create one with default values.
-        json_data = {"status": "Offline"};
-        text_data = json.dumps(json_data);
-        status_path.write_text(text_data);
-        logging.info("Status file created. Stopping script prematurely.");
-        sys.exit(0);
+        json_data = {"status": "Offline"} 
+        text_data = json.dumps(json_data) 
+        status_path.write_text(text_data) 
+        logging.info("Status file created. Stopping script prematurely.") 
+        sys.exit(0) 
 
 
 def update_status(status_path, twitch_data):
-    text_data = json.dumps(twitch_data);
-    status_path.write_text(text_data);
-    return;
+    text_data = json.dumps(twitch_data) 
+    status_path.write_text(text_data) 
+    return 
 
 
 def compare_status(current_data, prev_data):
     ## Just access keys directly and not loop through them
     if current_data["status"] == "Offline" and prev_data["status"] == "Offline":
-        print("Do nothing!");
+        print("Do nothing!") 
     elif current_data["status"] == "Offline" and prev_data["status"] == "Online":
-        print("We're going live!");
+        print("We're going live!") 
     elif current_data["status"] == "Online" and prev_data["status"] == "Offline":
-        print("He's done for the day");
+        print("He's done for the day") 
     else:
-        print("Probably hosting.");
-    return;
+        print("Probably hosting.") 
+    return 
 
 
 if __name__ == "__main__":
-    log_path, status_path = create_paths();
-    logging.FileHandler(filename=log_path, mode="a", encoding=None, delay=False);
-    logging.basicConfig(filename=log_path, format="%(asctime)s - %(message)s", level=logging.INFO);
-    logging.info("Starting script in " + os.path.basename(__file__));
+    log_path, status_path = create_paths() 
+    logging.FileHandler(filename=log_path, mode="a", encoding=None, delay=False) 
+    logging.basicConfig(filename=log_path, format="%(asctime)s - %(message)s", level=logging.INFO) 
+    logging.info("Starting script in " + os.path.basename(__file__)) 
 
-    current_twitch_data = jumjumjr.data();
-    prev_twitch_data = read_previous_status(status_path);
+    current_twitch_data = jumjumjr.data() 
+    prev_twitch_data = read_previous_status(status_path) 
 
-    update_status(status_path, current_twitch_data);
-    compare_status(current_twitch_data, prev_twitch_data);
-    logging.info("Exiting " + os.path.basename(__file__));
+    update_status(status_path, current_twitch_data) 
+    compare_status(current_twitch_data, prev_twitch_data) 
+    logging.info("Exiting " + os.path.basename(__file__)) 
 
-    logging.info("End of script.\n");
+    logging.info("End of script.\n") 
